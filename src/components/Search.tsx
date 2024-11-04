3a878c08052140fbc1a8f1712e6d164f2bf0e014
Naul <88250350+NaulNguyen@users.noreply.github.com>
2024-11-04 15:40:55 +0700

    search compont

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,91 +1,141 @@
-import React from "react";
-import { Header, Search } from "../components";
-import {
-    Box,
-    Container,
-    FormControl,
-    FormControlLabel,
-    FormLabel,
-    Radio,
-    RadioGroup,
-} from "@mui/material";
+import React, { useState } from "react";
+import { Autocomplete, Button, IconButton, InputAdornment, TextField } from "@mui/material";
+import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
+import MyLocationIcon from "@mui/icons-material/MyLocation";
+import PlaceIcon from "@mui/icons-material/Place";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
+import { useLocation } from "react-router-dom";
 
-const SearchingPage = () => {
+const cities = ["Hà Nội", "Hồ Chí Minh", "Đà Nẵng", "Hải Phòng", "Cần Thơ", "Nha Trang", "Huế"];
+
+const Search = () => {
+    const [departure, setDeparture] = useState<string | null>(null);
+    const [destination, setDestination] = useState<string | null>(null);
+    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+    const location = useLocation();
+
+    // Determine if the current page is the home page
+    const isHome = location.pathname === "/";
+
+    const handleSwap = () => {
+        const temp = departure;
+        setDeparture(destination);
+        setDestination(temp);
+    };
+
     return (
-        <div style={{ backgroundColor: "rgb(243,243,243)", height: "100vh" }}>
-            <Header />
-            <Container>
-                <Search />
-                <div className="pt-6 flex justify-center inset-0">
-                    <Box
-                        sx={{
-                            backgroundColor: "white",
-                            width: "25%",
-                            padding: "16px",
-                            display: "flex",
-                            flexDirection: "column",
-                            justifyContent: "space-between",
-                            borderRadius: "8px",
-                            borderWidth: "1px",
-                            borderColor: "rgb(209 213 219)",
-                        }}>
-                        <FormControl>
-                            <FormLabel
-                                id="sort-options-label"
-                                sx={{
-                                    fontWeight: "bold",
-                                    fontSize: "18px",
-                                    color: "black",
-                                    "&.Mui-focused": { color: "black" },
-                                }}>
-                                Sắp xếp
-                            </FormLabel>
-                            <RadioGroup
-                                aria-labelledby="sort-options-label"
-                                defaultValue="default"
-                                name="radio-buttons-group">
-                                <FormControlLabel
-                                    value="default"
-                                    control={<Radio />}
-                                    label="Mặc định"
-                                />
-                                <FormControlLabel
-                                    value="earliest"
-                                    control={<Radio />}
-                                    label="Giờ đi sớm nhất"
-                                />
-                                <FormControlLabel
-                                    value="latest"
-                                    control={<Radio />}
-                                    label="Giờ đi muộn nhất"
-                                />
-                                <FormControlLabel
-                                    value="priceAsc"
-                                    control={<Radio />}
-                                    label="Giá tăng dần"
-                                />
-                                <FormControlLabel
-                                    value="priceDesc"
-                                    control={<Radio />}
-                                    label="Giá giảm dần"
-                                />
-                            </RadioGroup>
-                        </FormControl>
-                    </Box>
+        <div
+            className={`${
+                isHome
+                    ? "absolute inset-0 flex justify-center items-center text-white text-3xl font-bold"
+                    : "w-full text-white text-3xl font-bold pt-6"
+            }`}>
+            {/* Search Section */}
+            <div
+                className={`${
+                    isHome
+                        ? "bg-white p-6 rounded-lg shadow-lg w-full max-w-5xl border border-gray-300"
+                        : "bg-white p-6 rounded-lg shadow-lg border border-gray-300"
+                }`}>
+                <div className="flex items-center space-x-4">
+                    {/* Nơi xuất phát */}
+                    <Autocomplete
+                        freeSolo
+                        options={cities}
+                        value={departure}
+                        onChange={(event: React.SyntheticEvent, newValue: string | null) => {
+                            setDeparture(newValue);
+                        }}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Nơi xuất phát"
+                                variant="outlined"
+                                fullWidth
+                                sx={{ minWidth: "220px" }}
+                                InputProps={{
+                                    ...params.InputProps,
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <MyLocationIcon sx={{ color: "blue" }} />
+                                        </InputAdornment>
+                                    ),
+                                }}
+                            />
+                        )}
+                    />
 
-                    <Box
-                        sx={{
-                            backgroundColor: "white",
-                            padding: "16px",
-                            borderRadius: "8px",
-                            borderWidth: "1px",
-                            borderColor: "rgb(209 213 219)",
-                            marginLeft: "16px", // Khoảng cách giữa các box
-                        }}></Box>
+                    {/* Swap Button */}
+                    <IconButton color="primary" onClick={handleSwap} aria-label="swap" size="large">
+                        <SwapHorizIcon />
+                    </IconButton>
+
+                    {/* Nơi đến */}
+                    <Autocomplete
+                        freeSolo
+                        options={cities}
+                        value={destination}
+                        onChange={(event: React.SyntheticEvent, newValue: string | null) => {
+                            setDestination(newValue);
+                        }}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Nơi đến"
+                                variant="outlined"
+                                fullWidth
+                                sx={{ minWidth: "220px" }}
+                                InputProps={{
+                                    ...params.InputProps,
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <PlaceIcon sx={{ color: "red" }} />
+                                        </InputAdornment>
+                                    ),
+                                }}
+                            />
+                        )}
+                    />
+
+                    {/* Thời gian */}
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DatePicker
+                            label="Chọn thời gian"
+                            value={selectedDate}
+                            onChange={(date) => setSelectedDate(date)}
+                            minDate={new Date()}
+                            disablePast
+                            slotProps={{
+                                textField: {
+                                    variant: "outlined",
+                                    fullWidth: true,
+                                    sx: { minWidth: "220px" },
+                                },
+                            }}
+                        />
+                    </LocalizationProvider>
+
+                    {/* Search Button */}
+                    <div className="flex justify-center ml-4 flex-row">
+                        <Button
+                            variant="contained"
+                            size="medium"
+                            sx={{
+                                minWidth: "120px",
+                                py: 2,
+                                backgroundColor: "rgb(255, 211, 51)",
+                                color: "black",
+                                textTransform: "none",
+                                fontSize: "16px",
+                            }}>
+                            Tìm kiếm
+                        </Button>
+                    </div>
                 </div>
-            </Container>
+            </div>
         </div>
     );
 };
 
-export default SearchingPage;
+export default Search;
