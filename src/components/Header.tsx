90a30dd75e8050a6ec960215bff23c6de47af9b7
Naul <88250350+NaulNguyen@users.noreply.github.com>
2024-11-07 15:51:02 +0700

    open menu when click into avatar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,35 @@
 import React, { useState } from "react";
-import { Button, Modal, Box, Avatar, styled, Badge } from "@mui/material";
+import { Button, Modal, Box, Avatar, styled, Badge, IconButton, Menu, MenuItem, ListItemIcon } from "@mui/material";
 import LoginIcon from "@mui/icons-material/Login";
 import Registration from "./Register";
 import Login from "./Login";
 import useAppAccessor from '../hook/useAppAccessor';
+import Logout from "@mui/icons-material/Logout";
+import PersonIcon from '@mui/icons-material/Person';
+import LoyaltyIcon from '@mui/icons-material/Loyalty';
+import CommentIcon from '@mui/icons-material/Comment';
+import Cookies from 'js-cookie';
+import { useDispatch } from "react-redux";
+import { logout } from '../actions/login.action';
+import { toast } from "react-toastify";
 
 const Header = () => {
     const [modalState, setModalState] = useState({ isRegisterOpen: false, isLoginOpen: false });
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const dispatch = useDispatch();
+    const open = Boolean(anchorEl);
+
     const { getUserInfor } = useAppAccessor(); 
     const userInfo = getUserInfor();
+
+    const handleAvatarClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleAvatarClose = () => {
+        setAnchorEl(null);
+    };
 
     const openModal = (type: any) => {
         setModalState({
@@ -48,19 +69,30 @@ const Header = () => {
         },
       }));
 
+    const handleLogout = () => {
+        Cookies.remove('accessToken');
+        Cookies.remove('refreshToken');
+        dispatch(logout());
+        toast.success("Đăng xuất thành công");
+    };
+
     return (
         <header className="flex justify-between items-center px-6 py-4 bg-white shadow-md">
             <div className="flex items-center">
                 <span className="font-pacifico text-4xl">TicketGo</span>
             </div>
             {userInfo.isAuthenticated ? (
-                <StyledBadge
-                overlap="circular"
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                variant="dot"
-              >
-                <Avatar src={userInfo.user.imageUrl} />
-                </StyledBadge>
+                <IconButton onClick={handleAvatarClick} 
+                    aria-controls={open ? 'account-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}>
+                    <StyledBadge
+                        overlap="circular"
+                        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                        variant="dot">
+                            <Avatar src={userInfo.user.imageUrl} />
+                    </StyledBadge>
+                </IconButton>
             ) : (
                 <div className="flex space-x-4">
                     <Button
@@ -73,6 +105,69 @@ const Header = () => {
                     </Button>
                 </div>
             )}
+            <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleAvatarClose}
+                onClick={handleAvatarClose}
+                slotProps={{
+                paper: {
+                    elevation: 0,
+                    sx: {
+                    overflow: 'visible',
+                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                    mt: 0.5,
+                    borderRadius: "10px",
+                    '& .MuiAvatar-root': {
+                        width: 32,
+                        height: 32,
+                        ml: -0.5,
+                        mr: 1,
+                    },
+                    '&::before': {
+                        content: '""',
+                        display: 'block',
+                        position: 'absolute',
+                        top: 0,
+                        right: 14,
+                        width: "fit-content",
+                        height: 10,
+                        bgcolor: 'background.paper',
+                        transform: 'translateY(-50%) rotate(45deg)',
+                        zIndex: 0,
+                    },
+                    },
+                },
+                }}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+                <MenuItem sx={{paddingX: "20px", paddingY: "10px"}}>
+                    <ListItemIcon>
+                        <PersonIcon fontSize="small" />
+                    </ListItemIcon>
+                    Thông tin tài khoản
+                </MenuItem>
+                <MenuItem sx={{paddingX: "20px", paddingY: "10px"}}>
+                    <ListItemIcon>
+                        <LoyaltyIcon fontSize="small" />
+                    </ListItemIcon>
+                    Đơn hàng của tôi
+                </MenuItem>
+                <MenuItem sx={{paddingX: "20px", paddingY: "10px"}} >
+                    <ListItemIcon>
+                        <CommentIcon fontSize="small" />
+                    </ListItemIcon>
+                    Nhận xét chuyến đi
+                </MenuItem>
+                <MenuItem sx={{paddingX: "20px", paddingY: "10px"}} onClick={handleLogout}>
+                    <ListItemIcon>
+                        <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Đăng xuất
+                </MenuItem>
+            </Menu>
             <Modal
                 open={modalState.isRegisterOpen}
                 onClose={closeModal}
