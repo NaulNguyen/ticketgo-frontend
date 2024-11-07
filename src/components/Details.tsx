b66454f1037d990600c47d6deda76484145fc3a0
Naul <88250350+NaulNguyen@users.noreply.github.com>
2024-11-07 15:51:37 +0700

    routeStop and policies

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,6 +1,5 @@
 import {
     Box,
-    Container,
     Divider,
     Grid,
     List,
@@ -9,24 +8,75 @@ import {
     Tabs,
     Typography,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TranslateIcon from "@mui/icons-material/Translate";
 import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
 import WaterDropIcon from "@mui/icons-material/WaterDrop";
 import HardwareIcon from "@mui/icons-material/Hardware";
 import BatteryChargingFullIcon from "@mui/icons-material/BatteryChargingFull";
-import CurtainsIcon from "@mui/icons-material/Curtains"; // Use a suitable icon
+import CurtainsIcon from "@mui/icons-material/Curtains"; 
 import SpeakerIcon from "@mui/icons-material/Speaker";
 import WifiIcon from "@mui/icons-material/Wifi";
 import AcUnitIcon from "@mui/icons-material/AcUnit";
-import BeachAccessIcon from "@mui/icons-material/BeachAccess";
+import axios from "axios";
 
-const Details = () => {
+interface RouteStop {
+    location: string;
+    arrivalTime: string;
+  }
+  
+interface RouteStopsData {
+    pickup: RouteStop[];
+    dropoff: RouteStop[];
+}
+
+interface DetailsProps {
+    scheduleId: string;
+}
+
+interface Policy {
+    policyType: string;
+    policyContent: string;
+}
+
+const Details: React.FC<DetailsProps> = ({ scheduleId }) => {
     const [tabIndex, setTabIndex] = useState(0);
+    const [routeStops, setRouteStops] = useState<RouteStopsData | null>(null);
+    const [policies, setPolicies] = useState<Policy[]>([]);
 
     const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
         setTabIndex(newValue);
     };
+
+    useEffect(() => {
+        const fetchRouteStops = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8080/api/v1/route-stops?scheduleId=${scheduleId}`);
+                setRouteStops(response.data.data);
+            } catch (error) {
+                console.error("Failed to fetch route stops", error);
+            }
+        };
+
+        if (scheduleId) {
+            fetchRouteStops();
+        }
+    }, [scheduleId]);
+
+    useEffect(() => {
+        if (tabIndex === 1) {
+            const fetchPolicies = async () => {
+                try {
+                    const response = await axios.get('http://localhost:8080/api/v1/policies');
+                    setPolicies(response.data.data); 
+                } catch (err) {
+                    console.error('Failed to fetch policies', err);
+                } 
+            };
+          fetchPolicies();
+        }
+      }, [tabIndex]);
+
 
     return (
         <Box>
@@ -62,10 +112,11 @@ const Details = () => {
             </Tabs>
 
             {/* Tab Content */}
-            <Box sx={{ p: 3, minHeight: "500px" }}>
-                {tabIndex === 0 && (
+            <Box sx={{ px: 3, pb: 3, minHeight: "500px" }}>
+                {tabIndex === 0 && routeStops && (
                     <Box>
-                        <Typography variant="h6" color="primary" fontSize={16} fontWeight={600}>
+                        <Divider />
+                        <Typography variant="h6" color="primary" fontSize={16} fontWeight={600} mt={2}>
                             Lưu ý
                         </Typography>
                         <Typography>
@@ -78,130 +129,93 @@ const Details = () => {
                                     variant="subtitle1"
                                     sx={{ mt: 2 }}
                                     fontSize={18}
-                                    fontWeight={700}>
+                                    fontWeight={700}
+                                >
                                     Điểm đón
                                 </Typography>
-                                <Typography>17:30 • Trạm Đà Lạt</Typography>
-                                <Typography>17:45 • Ga Đà Lạt</Typography>
-                                <Typography>18:15 • Vòng xoay Thái Phiên</Typography>
+                                <Box
+                                    sx={{
+                                        maxHeight: '440px', 
+                                        overflowY: 'auto',  
+                                        paddingRight: '10px', 
+                                    }}
+                                    >
+                                    {routeStops.pickup.map((stop, index) => {
+                                        const arrivalTime = new Date(stop.arrivalTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
+                                        return (
+                                        <Typography key={index} display="flex" alignItems="center" sx={{ paddingTop: '13px' }}>
+                                            <span style={{ fontWeight: 'bold', marginRight: '8px' }}>
+                                            {arrivalTime}
+                                            </span>
+                                            <span style={{ marginRight: '8px' }}>•</span>
+                                            <span style={{width : "190px"}}>{stop.location}</span>
+                                        </Typography>
+                                        );
+                                    })}
+                                </Box>
                             </Box>
                             <Box>
                                 <Typography
                                     variant="subtitle1"
                                     sx={{ mt: 2 }}
                                     fontSize={18}
-                                    fontWeight={700}>
+                                    fontWeight={700}
+                                >
                                     Điểm trả
                                 </Typography>
-                                <Typography>19:45 • Suối Đá Hòn Giao</Typography>
-                                <Typography>20:35 • Đèn giao thông Cầu Lùng</Typography>
-                                <Typography>20:40 • Trạm Nha Trang</Typography>
+                                <Box
+                                    sx={{
+                                        maxHeight: '440px', 
+                                        overflowY: 'auto',  
+                                        paddingRight: '10px', 
+                                    }}
+                                    >
+                                    {routeStops.dropoff.map((stop, index) => {
+                                        const arrivalTime = new Date(stop.arrivalTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
+                                        return (
+                                        <Typography key={index} display="flex" alignItems="center" sx={{ paddingTop: '13px' }}>
+                                            <span style={{ fontWeight: 'bold', marginRight: '8px' }}>
+                                            {arrivalTime}
+                                            </span>
+                                            <span style={{ marginRight: '8px' }}>•</span>
+                                            <span style={{width : "190px"}}>{stop.location}</span>
+                                        </Typography>
+                                        );
+                                    })}
+                                </Box>
                             </Box>
                         </Box>
                     </Box>
                 )}
 
-                {tabIndex === 1 && (
+                {tabIndex === 1 &&
                     <Box>
-                        <Typography variant="h6" fontSize={18} fontWeight={700}>
+                        <Divider />
+                        <Typography variant="h6" fontSize={18} fontWeight={700} mb={2} mt={3}>
                             Chính sách nhà xe
                         </Typography>
-                        <Typography
-                            variant="subtitle1"
-                            sx={{ mt: 2, color: "rgba(0, 0, 0, 0.65)" }}
-                            fontSize={16}
-                            fontWeight={700}>
-                            Yêu cầu khi lên xe
-                        </Typography>
-                        <List style={{ paddingLeft: "1.5rem", lineHeight: "1.6" }}>
-                            <ListItem>
-                                Có mặt tại văn phòng/quầy vé/bến xe trước 30 phút để làm thủ tục lên
-                                xe.
-                            </ListItem>
-                            <ListItem>Xuất trình SMS/Email đặt vé trước khi lên xe.</ListItem>
-                            <ListItem>Không mang đồ ăn, thức ăn có mùi lên xe.</ListItem>
-                            <ListItem>
-                                Không hút thuốc, uống rượu, sử dụng chất kích thích trên xe.
-                            </ListItem>
-                            <ListItem>Không mang các vật dễ cháy nổ lên xe.</ListItem>
-                            <ListItem>Không vứt rác trên xe.</ListItem>
-                            <ListItem>Không làm ồn, gây mất trật tự trên xe.</ListItem>
-                        </List>
-                        <Divider />
-                        <Typography
-                            variant="subtitle1"
-                            sx={{ mt: 2, color: "rgba(0, 0, 0, 0.65)" }}
-                            fontSize={16}
-                            fontWeight={700}>
-                            Hành lý xách tay
-                        </Typography>
-                        <List style={{ paddingLeft: "1.5rem", lineHeight: "1.6" }}>
-                            <ListItem>Tổng trọng lượng hành lý không vượt quá 15 kg</ListItem>
-                            <ListItem>Không vận chuyển hàng hóa cồng kềnh</ListItem>
-                            <ListItem>
-                                Không hoàn tiền trong trường hợp huỷ đơn hàng do vi phạm các quy
-                                định về hành lý
-                            </ListItem>
-                        </List>
-                        <Divider />
-                        <Typography
-                            variant="subtitle1"
-                            sx={{ mt: 2, color: "rgba(0, 0, 0, 0.65)" }}
-                            fontSize={16}
-                            fontWeight={700}>
-                            Trẻ em và phụ nữ có thai
-                        </Typography>
-                        <List style={{ paddingLeft: "1.5rem", lineHeight: "1.6" }}>
-                            <ListItem>
-                                Trẻ em dưới 4 tuổi hoặc dưới 100 cm được miễn phí vé nếu ngồi cùng
-                                ghế/giường với bố mẹ
-                            </ListItem>
-                            <ListItem>
-                                Trẻ em từ 4 tuổi hoặc cao từ 100 cm trở lên mua vé như người lớn
-                            </ListItem>
-                            <ListItem>
-                                Phụ nữ có thai cần đảm bảo sức khỏe trong suốt quá trình di chuyển
-                            </ListItem>
-                            <ListItem>
-                                Nhà xe có quyền từ chối phục vụ nếu hành khách không tuân thủ quy
-                                định về trẻ em và phụ nữ có thai
-                            </ListItem>
-                        </List>
-                        <Divider />
-                        <Typography
-                            variant="subtitle1"
-                            sx={{ mt: 2, color: "rgba(0, 0, 0, 0.65)" }}
-                            fontSize={16}
-                            fontWeight={700}>
-                            Động vật cảnh/Thú cưng
-                        </Typography>
-                        <List style={{ paddingLeft: "1.5rem", lineHeight: "1.6" }}>
-                            <ListItem>Nhà xe không nhận chở động vật cảnh/thú cưng</ListItem>
-                        </List>
-                        <Divider />
-                        <Typography
-                            variant="subtitle1"
-                            sx={{ mt: 2, color: "rgba(0, 0, 0, 0.65)" }}
-                            fontSize={16}
-                            fontWeight={700}>
-                            Xuất hóa đơn GTGT
-                        </Typography>
-                        <List style={{ paddingLeft: "1.5rem", lineHeight: "1.6" }}>
-                            <ListItem>Nhà xe không cung cấp hoá đơn GTGT</ListItem>
-                        </List>
-                        <Divider />
-                        <Typography
-                            variant="subtitle1"
-                            sx={{ mt: 2, color: "rgba(0, 0, 0, 0.65)" }}
-                            fontSize={16}
-                            fontWeight={700}>
-                            Gửi xe đạp/xe máy
-                        </Typography>
-                        <List style={{ paddingLeft: "1.5rem", lineHeight: "1.6" }}>
-                            <ListItem>Nhà xe không nhận gửi kèm xe đạp/xe máy.</ListItem>
-                        </List>
+                        {policies.length > 0 &&
+                            policies.map((policy, index) => (
+                                <>
+                                    <Typography
+                                        variant="subtitle1"
+                                        sx={{ mt: 2, color: "rgba(0, 0, 0, 0.65)" }}
+                                        fontSize={16}
+                                        fontWeight={700}
+                                        key={index}>
+                                        {policy.policyType}
+                                    </Typography>
+                                    <List style={{ paddingLeft: "1.5rem", lineHeight: "1.6" }}>
+                                        {policy.policyContent.split("\n").map((line, lineIndex) => (
+                                            <ListItem key={lineIndex}>{line}</ListItem>
+                                        ))}
+                                    </List>
+                                    <Divider />
+                                </>
+                            ))
+                        }
                     </Box>
-                )}
+                }
 
                 {tabIndex === 2 && (
                     <Box>
