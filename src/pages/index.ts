16a9eeddb2a53363bef6e691484c68cda6204f1e
Naul <88250350+NaulNguyen@users.noreply.github.com>
2024-11-14 17:21:19 +0700

    update import route

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,7 @@
 export { default as Home } from "./Home";
+export { default as BookingConfirm } from "./BookingConfirm";
+export { default as PaymentMethod } from "./PaymentMethod";
+export { default as ThankyouPage } from "./ThankyouPage";
+export { default as SearchingPage } from "./SearchingPage";
+
+
