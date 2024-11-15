48f09f09e8625dcf8323aaf022530ba598d76015
Naul <88250350+NaulNguyen@users.noreply.github.com>
2024-11-15 18:34:39 +0700

    adjust footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Box, Typography, Link, Divider } from '@mui/material';
+import { Box, Typography, Link } from '@mui/material';
 
 const Footer = () => {
   return (
     <Box
       sx={{
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        width: '100%',
         backgroundColor: 'white',
         padding: '20px 0',
         textAlign: 'center',
-        marginTop: 'auto',
       }}
     >
-      
       <Typography variant="body2" sx={{ color: 'black', marginBottom: '10px', fontWeight: "700", fontSize: "16px" }}>
         Công ty TNHH Thương Mại Dịch Vụ TicketGo
       </Typography>
@@ -24,7 +26,6 @@ const Footer = () => {
       <Typography variant="body2" sx={{ color: 'gray', marginBottom: '10px' }}>
         Giấy chứng nhận ĐKKD số 0315133726 do Sở KH và ĐT TP. Hồ Chí Minh cấp lần đầu ngày 27/6/2018
       </Typography>
-
 
       <Typography variant="body2" sx={{ color: 'gray' }}>
         Bản quyền © 2024 thuộc về <Link href="https://ticketgo.com" color="inherit" target="_blank" rel="noopener noreferrer">ticketgo.com</Link>
