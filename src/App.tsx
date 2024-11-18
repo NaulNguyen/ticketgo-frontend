ac782ca6e29ecbc6e72e11bbb5dc1eba93c367b2
Naul <88250350+NaulNguyen@users.noreply.github.com>
2024-11-18 15:12:00 +0700

    set up GoogleOAuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,35 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-import { BookingConfirm, BookingHistory, Home, PaymentMethod, SearchingPage, ThankyouPage } from "./pages";
+import { BookingConfirm, BookingHistory, Home, PaymentMethod, ResetPassword, SearchingPage, ThankyouPage } from "./pages";
 import AccountActivation from "./components/AccountActivation";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
     return (
-        <BrowserRouter>
-            <div>
-                <Provider store={store}>
-                    <div className="text-sm">
-                        <ToastContainer />
-                    </div>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/activate" element={<AccountActivation />} />
-                        <Route path="/search" element={<SearchingPage />} />
-                        <Route path="/booking-confirm" element={<BookingConfirm />} />
-                        <Route path="/payment-method" element={<PaymentMethod />} />
-                        <Route path="/thankyou" element={<ThankyouPage />} />
-                        <Route path="/booking-history" element={<BookingHistory />} />
-                    </Routes>
-                </Provider>
-            </div>
-        </BrowserRouter>
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || ""}>
+            <BrowserRouter>
+                <div>
+                    <Provider store={store}>
+                        <div className="text-sm">
+                            <ToastContainer />
+                        </div>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/activate" element={<AccountActivation />} />
+                            <Route path="/search" element={<SearchingPage />} />
+                            <Route path="/booking-confirm" element={<BookingConfirm />} />
+                            <Route path="/payment-method" element={<PaymentMethod />} />
+                            <Route path="/thankyou" element={<ThankyouPage />} />
+                            <Route path="/booking-history" element={<BookingHistory />} />
+                            <Route path="/reset-password" element={<ResetPassword />} />
+                        </Routes>
+                    </Provider>
+                </div>
+            </BrowserRouter>
+        </GoogleOAuthProvider>
     );
 }
 
