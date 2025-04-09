d1df62efc966ee8c4e69ce8e068b3dc0ca84f0db
Naul <88250350+NaulNguyen@users.noreply.github.com>
2025-04-09 23:07:24 +0700

    speed check

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,16 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import AppRoutes from "./routes/routes";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
     return (
-        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || ""}>
+        <GoogleOAuthProvider
+            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || ""}
+        >
             <BrowserRouter>
                 <div>
+                    <SpeedInsights />
                     <Provider store={store}>
                         <div className="text-sm">
                             <ToastContainer />
